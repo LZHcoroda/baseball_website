678b2154e1b3f7f770478b03edd20939c5d016ff
LZHreed <reedlimzh@gmail.com>
2022-08-31 18:06:29 +0800

    v0.1.2

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,35 @@
-import styles from '../styles/landing.module.scss'
-
-// className={`${styles.description} ${styles.yellow}`}
+// import styles from '../styles/landing.module.scss'
+import styles from '../styles/test.module.scss';
+import { RightOutlined } from '@ant-design/icons';
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.background}>
-      <h1>BASEBALL</h1>
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <div className={styles.imgBoxBB}>
-            <a href="/broken_bats">
-              <img className={styles.image} src='/images/Banner_BB.jpg'/>
-            </a>
-            <div className={styles.left}></div>
-            <div className={styles.right}></div>
+    <>
+      <div className={styles.nav}>Amateur Baseball Association - Singapore</div>
+      <div className={styles.background}> 
+        <div className={styles.container_left}>
+          <div className={styles.text}>
+            <h1 className={styles.title}>Broken Bats</h1>
+            <p className={styles.subtitle}>Amateur Team With The Will To Compete</p>
+            <br />
+            <Link href="/broken_bats/">
+              <a><button className={styles.btn}>Find Out More <RightOutlined /></button></a>
+            </Link>
           </div>
-        </div>
-        <div className={styles.box}>
-          <div className={styles.imgBoxBigs}>
-            <a href="/">
-              <img className={styles.image} src='/images/Banner_BB.jpg'/>
-            </a>
-            <div className={styles.left}></div>
-            <div className={styles.right}></div>
+        </div> 
+        <div className={styles.container_right}>
+          <div className={styles.text}>
+            <h1 className={styles.title}>BIGS</h1>
+            <p className= {styles.title2}>Baseball Interest Group - Singapore</p>
+            <p className={styles.subtitle}>Your Gateway Into The World Of Baseball</p>
+            <br />
+            <Link href="/bigs/">
+              <a><button className={styles.btn}>Find Out More <RightOutlined /></button></a>
+            </Link>
           </div>
-        </div>
-      </div> 
-     </div>
+        </div> 
+      </div>
+    </>
   )
 }
